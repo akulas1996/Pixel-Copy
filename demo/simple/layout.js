1e8529eca9eb654f69942534bcec81e3bacf83f4
sachin <akulas1996@gmail.com>
2019-01-10 14:17:20 -0800

    smooth scroll and send button

--- demo/simple/layout.js
+++ demo/simple/layout.js
@@ -19,18 +19,22 @@
   $('.wrapper').hide();
   
     var ENTER_KEY_CODE = 13;
-    var queryInput, resultDiv, accessTokenInput;
+    var queryInput, resultDiv, accessTokenInput, sendButton;
   
     window.onload = init;
   
     function init() {
       queryInput = document.getElementById("q");
+      sendButton = document.getElementById("click");
       resultDiv = document.getElementById("msg-insert");
       accessTokenInput = document.getElementById("access_token");
       var setAccessTokenButton = document.getElementById("set_access_token");
   
       queryInput.addEventListener("keydown", queryInputKeyDown);
+      sendButton.addEventListener("click", onSendButtonClick);
       setAccessTokenButton.addEventListener("click", setAccessToken);
+      
+
   
       window.init(accessTokenInput.value);
       
@@ -80,10 +84,68 @@
   
     }
 
+    function onSendButtonClick(event) {
+
+      console.log(event)
+  
+      var value = queryInput.value;
+      queryInput.value = "";
+  
+      createQueryNode(value);
+      var responseNode = createResponseNode();
+      
+  
+      sendText(value)
+        .then(function(response) {
+          var result;
+          var link = ""
+          try {
+            if(response.result.fulfillment.speech === ""){
+  
+            if("data" in response.result.fulfillment) {
+              console.log("IN LAMBDA")
+              result = response.result.fulfillment.data.google.richResponse.items[0].simpleResponse.textToSpeech;
+              if(response.result.fulfillment.data.google.richResponse.items[0].simpleResponse.link = ""){
+                var linkInformation = "Click for more information";
+                var link = "http://pixelarchitect.ca";
+                var extraNode = createLinkButtonNode(); 
+                setButtonLinkNodes(linkInformation, extraNode, link);
+              }
+  
+            }else {
+  
+              console.log("IN CHATBOT")
+              result = response.result.fulfillment.messages[0].payload.chatbot.Message;
+              var linkInformation = "Click for more information";
+              var link = response.result.fulfillment.messages[0].payload.chatbot.link;
+              console.log("THIS IS LINK " + link)
+              var extraNode = createLinkButtonNode(); 
+              setButtonLinkNodes(linkInformation, extraNode, link);
+            }
+            }
+            else {
+            result = response.result.fulfillment.speech
+            }
+          } catch(error) {
+            result = "Please try again. You can say help, to find out what I can do.";
+          }
+          setResponseJSON(response);
+          
+          setResponseOnNode(result, responseNode, link);
+        })
+        .catch(function(err) {
+          setResponseJSON(err);
+          var link="http://pixelarchitect.ca"
+          setResponseOnNode("Something goes wrong", responseNode, link);
+        });
+    }
+  
+
   function queryInputKeyDown(event) {
     if (event.which !== ENTER_KEY_CODE) {
       return;
     }
+    console.log(event)
 
     var value = queryInput.value;
     queryInput.value = "";
@@ -195,31 +257,18 @@
     node.innerHTML = response ? response : "[empty response]";
 
     response = response.replace(/\n/g, "<br />");
-    //console.log("THis is response from setResponseNode "  + response)
-    //response = "<a href="+link+">" +response +"</a>";
+
     node.innerHTML = response;
     node.style.textAlign = "left";
-
     node.className = "msg-receive";
-    // var iconNode = document.createElement("i");
-    // iconNode.className = "fas fa-user";
-    //iconNode.style="float:right; background-color:black; font-size:40px; padding:5px; z-index:0";
-    // iconNode.style.cssFloat = "left";
-    // iconNode.style.display = "inline-block";
-    //iconNode.style.height = "50px";
-    //iconNode.style.width = "50px";
-    // iconNode.style.backgroundColor = "#e9ecef";
-    // iconNode.style.color = "black";
-    // iconNode.style.zIndex="10000";
-    // iconNode.style.position="relative";
-    // iconNode.style.textAlign="left";
-    // iconNode.style.padding="10px"
-    //resultDiv.appendChild(iconNode);
     resultDiv.appendChild(node);
-
-
     var d = $('.chat-body');
-    d.scrollTop(d.prop("scrollHeight"));
+    console.log(d.height())
+    //d.scrollTop(d.prop("scrollHeight"));
+    // d.animate({scrollTop: 1000}, 2000);
+
+    d.animate({ scrollTop: 3000 }, 3000);
+
     
 
 
